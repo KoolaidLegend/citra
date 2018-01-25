b87ca8ab2570021501f24780eb25efc60ee83344
Jhon591 <mellori246@gmail.com>
2018-01-25 14:45:48 +0000

    JFR_BG_Colour

--- src/citra_qt/configuration/configure_graphics.cpp
+++ src/citra_qt/configuration/configure_graphics.cpp
@@ -2,6 +2,8 @@
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
+#include <QColorDialog>
+
 #include "citra_qt/configuration/configure_graphics.h"
 #include "core/core.h"
 #include "core/settings.h"
@@ -18,6 +20,16 @@ ConfigureGraphics::ConfigureGraphics(QWidget* parent)
     connect(ui->toggle_frame_limit, &QCheckBox::stateChanged, ui->frame_limit, &QSpinBox::setEnabled);
 
     ui->layoutBox->setEnabled(!Settings::values.custom_layout);
+
+    connect(ui->layout_bg, SIGNAL(released()), this, SLOT(showLayoutBackgroundDialog()));
+}
+
+void ConfigureGraphics::showLayoutBackgroundDialog() {
+    QColor new_color = QColorDialog::getColor(bg_color, this);
+    if (new_color.isValid()) {
+        bg_color = new_color;
+        ui->layout_bg->setStyleSheet("QPushButton { background-color: " + bg_color.name() + ";}");
+    }
 }
 
 ConfigureGraphics::~ConfigureGraphics() {}
@@ -28,6 +40,11 @@ void ConfigureGraphics::setConfiguration() {
     ui->toggle_shader_jit->setChecked(Settings::values.use_shader_jit);
     ui->resolution_factor_combobox->setCurrentIndex(Settings::values.resolution_factor);
     ui->toggle_vsync->setChecked(Settings::values.use_vsync);
+    {
+        bg_color.setRgbF(Settings::values.bg_red, Settings::values.bg_green,
+                         Settings::values.bg_blue);
+        ui->layout_bg->setStyleSheet("QPushButton { background-color: " + bg_color.name() + ";}");
+    }
     ui->toggle_frame_limit->setChecked(Settings::values.use_frame_limit);
     ui->frame_limit->setValue(Settings::values.frame_limit);
     ui->layout_combobox->setCurrentIndex(static_cast<int>(Settings::values.layout_option));
@@ -40,6 +57,9 @@ void ConfigureGraphics::applyConfiguration() {
     Settings::values.resolution_factor =
         static_cast<u16>(ui->resolution_factor_combobox->currentIndex());
     Settings::values.use_vsync = ui->toggle_vsync->isChecked();
+    Settings::values.bg_red = bg_color.redF();
+    Settings::values.bg_green = bg_color.greenF();
+    Settings::values.bg_blue = bg_color.blueF();
     Settings::values.use_frame_limit = ui->toggle_frame_limit->isChecked();
     Settings::values.frame_limit = ui->frame_limit->value();
     Settings::values.layout_option =
